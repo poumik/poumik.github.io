17b69953a1a530e2b9777345dc8c7b0500040d54
Mika Pouramo <74448485+poumik@users.noreply.github.com>
2021-06-04 01:48:42 +0300

    Update 3viikonensimmainen.js

--- ohjelmointi/js/3viikonensimmainen.js
+++ ohjelmointi/js/3viikonensimmainen.js
@@ -116,7 +116,7 @@ function muokkaaSalasana()
   var eka, toka;
   pw = document.getElementById('salis').value;
   alert(pw);
-  var taulu = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'x', 'y', 'z', 'Ã¥', 'Ã¤', 'Ã¶', 'w'];
+  var taulu = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'x', 'y', 'z', 'w'];
   var temp = [];
   var lop='';
   for(var s = 0; s < pw.length; s++)
